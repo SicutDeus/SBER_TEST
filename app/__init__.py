f557d7973b90b485556310a5aab5edfd9d2fd297
m1sha <m.prisyazhnyy@storageup.net>
2024-08-22 12:34:05 +0300

    final

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,10 @@
+from flasgger import Swagger
 from flask import Flask
-
 from app.routes import deposit_api
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    swagger = Swagger(app, template_file='swagger.yml')
     app.register_blueprint(deposit_api)
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,17 @@
 from typing import Any, Dict, Tuple, Union
 
+from flasgger import swag_from
 from flask import Blueprint, Response, jsonify, request
 
 from app.config import BASE_ERROR_STATUS_CODE, BASE_SUCCESS_STRATUS_CODE
 from app.response import get_unsorted_json_response
 from app.utils import calculate_deposit
 from app.validation import DepositRequestSchema
-
 deposit_api = Blueprint("deposit_api", __name__)
 
 
 @deposit_api.route("/calculate", methods=["POST"])
+@swag_from("swagger.yml", validation=True)
 def calculate() -> Union[Response, Tuple[Response, int]]:
     """
     Обрабатывает POST-запрос для расчета депозита.
